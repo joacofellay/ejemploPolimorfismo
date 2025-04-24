28b323394c64d80f44fa613b9904e74085512f56
Joaquin Fellay <joafellay@gmail.com>
2025-04-23 23:17:07 -0300

    first commit

---
+++ lib/Sensor/Sensor.h
@@ -0,0 +1,13 @@
+#ifndef SENSOR_H
+#define SENSOR_H
+#include <Arduino.h>
+
+class Sensor {   // Clase base para los sensores
+    public:
+    Sensor(int pin);// Constructor
+    ~Sensor();// Destructor
+    virtual void configurar() = 0; // Configura el sensor
+    virtual void getValor() = 0; // Obtiene el valor del sensor
+    virtual String getNombre() = 0; // Obtiene el nombre del sensor   
+};
+#endif
